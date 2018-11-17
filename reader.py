3f36697224effa06378c5c5908ef7e8bd22824a8
Shreyanshu Shekhar <36325564+shreyanshu007@users.noreply.github.com>
2018-11-17 14:12:50 +0530

    1

--- reader.py
+++ reader.py
@@ -1,4 +1,5 @@
 import csv 
+import nltk
 
 f = open('train.csv', encoding="utf8")
 
@@ -6,11 +7,18 @@ reader = csv.reader(f, delimiter=',')
 
 i= 0
 tag_list = []
+word_list = []
 for line in reader:
 	# print(line[1])
 	# if i > 1000:
 	# 	break
-	# print(i)
+	print(i)
+	# print (line[0])
+	for wro in nltk.pos_tag(line[0].split()):
+		# print(nltk.pos_tag(line[0].split()))
+		# print("chek")
+		if(wro[1] == 'NNP'):
+			word_list.append(wro[0])
 	i += 1
 	l = line[1]
 	l = l[2:len(l)-2]
@@ -22,7 +30,16 @@ for line in reader:
 		tag_list.append(tag)
 
 l = list(set(tag_list))
+m = list(set(word_list))
 
 print(i)
 print(len(l))
 print(l)
+
+
+# print(m)
+print(len(m))
+print((m))
+# print(m.index("the"))
+# print(m.index("a"))
+# print(m.index("is"))
